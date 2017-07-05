04ded0913ba0546eb240a61e8e8d589b0c4ed3c6
Atli Egilsson <atli333@gmail.com>
2017-07-05 11:56:34 +0000

    Changed back because of console errors and because I am a perfectionist.

---
+++ WebApplication/Scripts/AlphabetSurvey.js
@@ -0,0 +1,21 @@
+﻿// Create instance of the colorWheel
+var colorWheel = iro.ColorWheel("#colorWheel", {
+    width: 320,
+    height: 320,
+    padding: 4,
+    markerRadius: 8,
+    color: "rgb(68, 255, 158)",
+    css: {
+        "#logo": {
+            "stroke": "rgb"
+        },
+        ".subtitle": {
+            "color": "rgb"
+        }
+    }
+});
+
+// Change the color of the character along with the colorwheel
+colorWheel.watch(function (color) {
+    $('#char p').css('color', color.hexString);
+});
